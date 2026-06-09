0806f42e4ff4fb65097c1fc49c858db1a1d4c2df
Intel AI pc <INTEL-AI-PC@AI.com>
2026-06-09 13:58:13 +0300

    testing new chagnes in github

--- src/Server.py
+++ src/Server.py
@@ -161,6 +161,6 @@ class DB_API:
         except Exception as e:
             print(f"Aggregation query failed: {e}")
             return None
-        
+        #this is a test
 if __name__ == "__main__":
     print("hola")
