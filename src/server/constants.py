a295b1991097b5ec88b27087219b4e53076852a2
RenderUpdate <104227801+RealShiroTheShiro@users.noreply.github.com>
2026-06-11 15:47:47 +0300

    small fix

--- src/server/constants.py
+++ src/server/constants.py
@@ -12,7 +12,7 @@ RAMBAM_DEPARTMENTS_LIST = [
     "cardiac intensive care unit", "maternity and gynecology", "general surgery a",
     "general surgery b", "pediatric surgery", "plastic surgery",
     "cardiothoracic surgery", "vascular surgery", "oral and maxillofacial surgery",
-    "neurology", "pediatrics pip install --upgrade openaia", "pediatrics b", "nephrology", "neurosurgery",
+    "neurology", "pediatrics", "pediatrics b", "nephrology", "neurosurgery",
     "dermatology", "ophthalmology", "internal medicine a", "internal medicine b",
     "internal medicine c", "internal medicine d", "internal medicine e",
     "internal medicine f", "psychiatry", "neonatal intensive care unit (nicu)",
