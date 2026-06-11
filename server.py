417a6f38d39422bd4d64f99d1a032ea9fc2f80b7
RenderUpdate <104227801+RealShiroTheShiro@users.noreply.github.com>
2026-06-11 19:53:54 +0300

    reordering voice ask and text ask + a few test

--- server.py
+++ server.py
@@ -3,45 +3,43 @@ import traceback
 from llm_agent import ask_assistant
 from src.server.DB_API import DB_API
 from src.server.constants import PRESET_QUERIES
+from src.llms.voice_to_text import transcribe
 
 
 app = Flask(__name__)
 db = DB_API()
 
-@app.route('/api/ask', methods=['POST'])
-def handle_ask_assistant():
-    data = request.json
+
+def processApiRequest(request, message):
     try:
-        if not data.get("request").isnumeric():
+        if type(request) != int:
             return jsonify({"error": "Request must be numeric."}), 500
-        user_request = int(data.get("request"))
-        user_message = data.get("message")
+        user_request = int(request)
+        user_message = message
     except Exception as e:
         print(f" ERROR in /api/ask: {e}")
         traceback.print_exc()
         return jsonify({"error": str(e)}), 500
 
-    if not user_message or not user_request:
-        return jsonify({"error": "Missing payload. Please provide with both the fields \"request\" and \"message\"."}), 400
     try:
         response = None
-        if user_request == PRESET_QUERIES.Locate_Patient:
-            if not data.get("message").isnumeric():
+        if user_request == PRESET_QUERIES.Locate_Patient.value:
+            if not message.isnumeric():
                 return jsonify({"error": "Patient number must be numeric."}), 500
             response = db.SearchForPatient(int(user_message))
-        elif user_request == PRESET_QUERIES.Room_Occupancy:
+        elif user_request == PRESET_QUERIES.Room_Occupancy.value:
             response = db.GetRoomPatientsCount(user_message)
-        elif user_request == PRESET_QUERIES.Total_Occupancy:
+        elif user_request == PRESET_QUERIES.Total_Occupancy.value:
             response = db.GetTotalPatientsCount()
-        elif user_request == PRESET_QUERIES.Administration_Time:
-            if not data.get("message").isnumeric():
+        elif user_request == PRESET_QUERIES.Administration_Time.value:
+            if not message.isnumeric():
                 return jsonify({"error": "Patient number must be numeric."}), 500
             response = db.GetAdministrationTime(int(user_message))
-        elif user_request == PRESET_QUERIES.Idle_Time:
-            if not data.get("message").isnumeric():
+        elif user_request == PRESET_QUERIES.Idle_Time.value:
+            if not message.isnumeric():
                 return jsonify({"error": "Patient number must be numeric."}), 500
             response = db.GetIdleTime(int(user_message))
-        elif user_request == PRESET_QUERIES.Other:
+        elif user_request == PRESET_QUERIES.Other.value:
             response = ask_assistant(user_message)
         else:
             response = "Unknown request. Please provide a request from the allegeable list."
@@ -50,6 +48,73 @@ def handle_ask_assistant():
         print(f" ERROR in /api/ask: {e}")
         traceback.print_exc()
         return jsonify({"error": str(e)}), 500
+
+
+@app.route('/api/ask/voice', methods=['POST'])
+def handle_ask_assistant_voice():
+    audio_bytes = request.data
+
+    with open("received.ogg", "wb") as f:
+        f.write(audio_bytes)
+    
+    with open("received.ogg", "r") as f:
+        header = f.read(4)
+        f.seek(0)
+
+        if header != b"OggS":
+            return {"error": "Invalid OGG file"}, 400
+
+
+    stt = transcribe("received.ogg", language="he")
+
+    return processApiRequest(stt['text'], PRESET_QUERIES.Other)
+
+    
+@app.route('/api/ask', methods=['POST'])
+def handle_ask_assistant():
+    data = request.json
+
+    if not data.get("request") or not data.get("message"):
+        return jsonify({"error": "Missing payload. Please provide with both the fields \"request\" and \"message\"."}), 400
+
+    return processApiRequest(data.get("request"), data.get("message"))
+    # try:
+    #     if not data.get("request").isnumeric():
+    #         return jsonify({"error": "Request must be numeric."}), 500
+    #     user_request = int(data.get("request"))
+    #     user_message = data.get("message")
+    # except Exception as e:
+    #     print(f" ERROR in /api/ask: {e}")
+    #     traceback.print_exc()
+    #     return jsonify({"error": str(e)}), 500
+
+    # try:
+    #     response = None
+    #     if user_request == PRESET_QUERIES.Locate_Patient:
+    #         if not data.get("message").isnumeric():
+    #             return jsonify({"error": "Patient number must be numeric."}), 500
+    #         response = db.SearchForPatient(int(user_message))
+    #     elif user_request == PRESET_QUERIES.Room_Occupancy:
+    #         response = db.GetRoomPatientsCount(user_message)
+    #     elif user_request == PRESET_QUERIES.Total_Occupancy:
+    #         response = db.GetTotalPatientsCount()
+    #     elif user_request == PRESET_QUERIES.Administration_Time:
+    #         if not data.get("message").isnumeric():
+    #             return jsonify({"error": "Patient number must be numeric."}), 500
+    #         response = db.GetAdministrationTime(int(user_message))
+    #     elif user_request == PRESET_QUERIES.Idle_Time:
+    #         if not data.get("message").isnumeric():
+    #             return jsonify({"error": "Patient number must be numeric."}), 500
+    #         response = db.GetIdleTime(int(user_message))
+    #     elif user_request == PRESET_QUERIES.Other:
+    #         response = ask_assistant(user_message)
+    #     else:
+    #         response = "Unknown request. Please provide a request from the allegeable list."
+    #     return jsonify({"response": response}), 200
+    # except Exception as e:
+    #     print(f" ERROR in /api/ask: {e}")
+    #     traceback.print_exc()
+    #     return jsonify({"error": str(e)}), 500
     
 @app.route('/api/update_location', methods=['POST'])
 def handle_update_location():
